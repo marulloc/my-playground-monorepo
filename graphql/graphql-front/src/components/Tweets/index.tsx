03c3b0410081de28ed482c9b9d01b448bc703d5e
marulloc <marullo@naver.com>
2023-04-03 01:31:33 +0900

    make query Client code to declarative code

--- graphql/graphql-front/src/components/Tweets/index.tsx
+++ graphql/graphql-front/src/components/Tweets/index.tsx
@@ -1,29 +1,42 @@
-import { gql, useApolloClient } from '@apollo/client';
-import { useEffect, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_TWEETS = gql`
+    query getAllMovies {
+        allTweets {
+            id
+            text
+        }
+    }
+`;
 
 const Tweets = () => {
-    const client = useApolloClient();
-    const [tweets, setTweets] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const { data } = await client.query({
-                query: gql`
-                    {
-                        allTweets {
-                            id
-                            text
-                        }
-                    }
-                `,
-            });
-            setTweets(data.allTweets);
-        })();
-    }, [client]);
+    // const client = useApolloClient();
+    // const [tweets, setTweets] = useState([]);
 
+    const { data, loading, error } = useQuery(ALL_TWEETS);
+
+    // useEffect(() => {
+    //     (async () => {
+    //         const { data } = await client.query({
+    //             query: gql`
+    //                 {
+    //                     allTweets {
+    //                         id
+    //                         text
+    //                     }
+    //                 }
+    //             `,
+    //         });
+    //         setTweets(data.allTweets);
+    //     })();
+    // }, [client]);
+
+    if (loading) return <h1>Loading </h1>;
+    if (error) return <h1>Error</h1>;
     return (
         <section>
             <ul>
-                {tweets.map((tweet) => (
+                {data.allTweets.map((tweet) => (
                     <li key={tweet?.id}>
                         <p>{tweet?.text}</p>
                     </li>
