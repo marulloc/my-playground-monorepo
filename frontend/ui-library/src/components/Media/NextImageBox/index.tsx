87acf5270aef8a9f5116ff8538e1742e604cc9a8
marulloc <marullo@naver.com>
2023-04-13 23:53:01 +0900

    migration to Next13-Tailwind repo

--- frontend/ui-library/src/components/Media/NextImageBox/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-import Image from 'next/image';
-
-const NextImageBox = styled(Image)`
-    height: auto !important;
-    position: relative !important;
-`;
-
-export default NextImageBox;
